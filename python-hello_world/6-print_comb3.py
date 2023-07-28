ea052100d8c4dc66f1bca860eff253c98b9ca2c4
DevBrightly <itsgeorgebright.com>
2023-07-28 03:29:08 +0100

    updato

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,1 @@
-for number in range(10):
-    for number2 in range(number + 1, 10):
-        print("{:02}".format(number * 10 + number2), end=", " if number != 8 else " ")
+print(", ".join("{:02}".format(number * 10 + number2) for number in range(10) for number2 in range(number + 1, 10)))
