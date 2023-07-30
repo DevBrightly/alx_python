6db334affa5032e3c23ca71419d346e324f52c57
DevBrightly <itsgeorgebright.com>
2023-07-30 04:38:46 +0100

    addition

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
-# main.py
+# 0-add.py
 
 a = 1
 b = 2
 
-from add_0 import add
+# Use __import__ to import the function
+add_module = __import__('add_0')
+add = add_module.add
 
 result = add(a, b)
 
