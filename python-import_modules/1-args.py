2bc7e7986eb67dcc38e58060e69ee36cc85cfa7b
DevBrightly <itsgeorgebright.com>
2023-08-01 16:40:36 +0100

    args

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,14 +4,14 @@ def print_arguments():
  
     num_args = len(sys.argv) - 1
 
-    print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
     
     if num_args > 0:
+        print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
         # print("Argument{}:".format('s' if num_args > 1 else ''))
         for i, arg in enumerate(sys.argv[1:], start=1):
             print("{}: {}".format(i, arg))
     else:
-        print(".")
+        print("0 arguments.")
     
 if __name__ == "__main__":
     print_arguments()
