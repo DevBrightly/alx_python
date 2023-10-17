bea6889ac649e6a22b8dff46777c7d990ec51e14
DevBrightly <itsgeorgebright.com>
2023-10-17 21:06:53 +0100

    printing from 00 - 99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,1 +1,5 @@
-print(", ".join("{:02}".format(number) for number in range(100)))
+for number in range(100):
+    if number < 99:
+        print("{:02}".format(number), end=', ')
+    else:
+        print("{:02}".format(number), end='\n')
