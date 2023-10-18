f420362d02247bcfd7dd29d6160d0ce1b7bf74e1
DevBrightly <itsgeorgebright.com>
2023-10-18 01:28:46 +0100

    solution updated

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,1 +1,7 @@
-print(", ".join("{:02}".format(number * 10 + number2) for number in range(10) for number2 in range(number + 1, 10)))
+for tens_digit in range(10):
+    for unit_digit in range(tens_digit, 10):
+        if tens_digit != unit_digit:
+            if tens_digit < 8:
+                print("{}{}".format(tens_digit, unit_digit), end=', ')
+            else:
+                print("{}{}".format(tens_digit, unit_digit), end='\n')
